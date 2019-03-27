178bbe9037625b709ef83e42c291e35dd47be28f
EdwardGao <1254339809@qq.com>
2019-03-27 14:56:07 +0800

    update user name

--- NormalProject/AMContainer.h
+++ NormalProject/AMContainer.h
@@ -2,8 +2,8 @@
 //  AMContainer.h
 //  NormalProject
 //
-//  Created by 李宁锋 on 2019/3/21.
-//  Copyright © 2019 李宁锋. All rights reserved.
+//  Created by gyh on 2019/3/21.
+//  Copyright © 2019 gyh. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
